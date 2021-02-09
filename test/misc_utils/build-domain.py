ee671763bfaf87227d2cb026f2e408f92981f1a4
Saumitra Sapre <saumitrasapre69@gmail.com>
2021-02-10 02:11:42 +0530

    Directory restructure

--- test/misc_utils/build-domain.py
+++ test/misc_utils/build-domain.py
@@ -13,5 +13,5 @@ for filename in glob.glob(os.path.join(path, '*.yml')):
 merged_yaml = hiyapyco.load(yaml_list, method=hiyapyco.METHOD_MERGE)
 print(hiyapyco.dump(merged_yaml))
 
-domain_yml_file = open("domain.yml", "w+")
+domain_yml_file = open("../domain.yml", "w+")
 domain_yml_file.writelines(hiyapyco.dump(merged_yaml))
